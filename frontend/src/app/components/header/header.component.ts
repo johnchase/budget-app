e43987e0e96c16e6f35b47a52c62f11116d08a6f
John Chase <chasejohnh@gmail.com>
2020-01-10 16:48:13 -0800

    added logout button

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service'
+import { User } from 'src/app/interfaces/user.model'
 
 @Component({
   selector: 'app-header',
@@ -9,16 +11,25 @@ import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   public currPage = '/overview';
+  currentUser: User;
 
   constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
     private _router: Router,
   ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     // Listen for page changes, highlight the sidebar link that is the current one
     this._router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         this.currPage = event.url;
       }
     })
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
   ngOnInit() {
