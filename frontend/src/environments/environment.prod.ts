51724b0d8c4313977f188d6c5989583257f1e294
John Chase <chasejohnh@gmail.com>
2020-01-11 09:50:50 -0800

    changed url again

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  API_URL: 'budget-env.k7ewbbgffa.us-west-1.elasticbeanstalk.com',
+  production: true,
+  API_URL: 'http://budget-env.k7ewbbgffa.us-west-1.elasticbeanstalk.com',
   REDIRECT_URL: 'https://budget.johnhchase.com/overview',
 };
