ff9bd40acd4392cce678495e212e7dd5f6692203
John Chase <chasejohnh@gmail.com>
2020-02-29 10:25:46 -0800

    added category data

---
+++ frontend/src/app/data/category_data.ts
@@ -0,0 +1,11 @@
+export const categories: Object = {
+  Restaurants: "fas fa-utensils",
+  Health: "fas fa-medkit",
+  Entertainment: "fas fa-ticket-alt",
+  Gas: "fas fa-gas-pump",
+  Groceries: "fas fa-shopping-cart",
+  Items: "fas fa-store",
+  Rideshare: "fab fa-uber",
+  Clothing: "fas fa-tshirt",
+  Haircut: "fas fa-cut"
+};
